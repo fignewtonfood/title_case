e33f161cd76981446f546ad319cca532920ead0c
Tim White & Jason Bethel <student@epicodus.com>
2015-08-10 13:48:30 -0700

    complete method to pass fourth spec

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -9,25 +9,15 @@
             $lc_output = array();
             foreach ($input_words_as_array as $word) {
                 array_push($lc_output, strtolower($word));
-                array_push($output, ucfirst($word));
             }
-            // $intersect = array_intersect($array_small_words, $lc_output);
-            // if (!empty($intersect)) {
             foreach ($lc_output as $lc) {
-
-                // foreach ($intersect as $intersection) {
-                if(in_array($lc, $array_small_words)) {
+                if(!in_array($lc, $array_small_words)) {
                     $lc = ucfirst($lc);
                 }
                 array_push($output, $lc);
-                // }
             }
-            // } else {
-            //     return implode (" ", $output);
-            // }
+            return implode (" ", $output);
+
         }
     }
 ?>
-
-
-a, an, the, at, by, for, in, of, on, to, up, and, as, but, or, nor
